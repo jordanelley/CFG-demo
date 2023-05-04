2e2c2f3e6e349f1dc17bc100ca25ac755c93f9be
Jordan <jordanelley@hotmail.com>
2023-05-05 05:52:00 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function pressAlert(){
+    alert("button has been pressed")
+}
+
+
+document.getElementById("button1").addEventListener("click", pressAlert);
+document.getElementById("img1").addEventListener("click",pressAlert);
+
